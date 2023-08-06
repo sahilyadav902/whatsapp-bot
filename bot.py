b881ef2a29094b68ef7281d5b39a73a7c5be6c9c
Sahil <sahilyadav902@gmail.com>
2023-08-06 19:00:38 +0530

    render.com test

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-from embedchain import App
+# from embedchain import App
+from embedchain.apps.App import App
 
 app = Flask(__name__)
 chat_bot = App()
